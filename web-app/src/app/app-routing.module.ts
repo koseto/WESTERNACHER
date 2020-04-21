6cf4a7094c3ed44c685e2dfaf0385e301855fbab
Kose <bochevakostantsa@gmail.com>
2020-04-21 18:39:31 +0300

    add routing, common styles, change project to use SCSS instead of css

---
+++ web-app/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UserLookupComponent} from './user-lookup/user-lookup.component';
+
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UserLookupComponent
+  },
+  {
+    path: '',
+    redirectTo: '/users',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -2,16 +2,22 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {UserLookupComponent} from './user-lookup/user-lookup.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    UserLookupComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }

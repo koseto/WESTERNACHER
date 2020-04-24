7b26c280d6cd0702cd8638b5b37cd0f991358e13
Kose <bochevakostantsa@gmail.com>
2020-04-24 06:11:37 +0300

    use ngx-datatable for account results

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
 import {UserLookupComponent} from './user-lookup/user-lookup.component';
 import {AppRoutingModule} from './app-routing.module';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {AppRoutingModule} from './app-routing.module';
   imports: [
     BrowserModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxDatatableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web-app/src/app/user-lookup/user-lookup.component.ts
+++ web-app/src/app/user-lookup/user-lookup.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user-lookup',
@@ -7,7 +8,9 @@ import {Component, OnInit} from '@angular/core';
 })
 export class UserLookupComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    private _modalService: NgbModal
+  ) {
   }
 
   ngOnInit(): void {
